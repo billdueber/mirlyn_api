ddaa8b2d047741fedd276510517ee037913e48f2
Bill Dueber <bill@dueber.com>
2014-08-25 12:06:22 -0400

    First commit

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+source 'http://gems.www.lib.umich.edu'
+
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'mirlyn_id_api'
+gem 'json'
+
+
---
+++ simple_mirlyn_api.rb
@@ -0,0 +1,40 @@
+require 'sinatra/base'
+require 'sinatra/config_file'
+require 'sinatra/json'
+require 'json'
+require 'mirlyn_id_api'
+
+class SimpleMirlynAPI < Sinatra::Base
+
+  register Sinatra::ConfigFile
+
+  config_file File.join(File.dirname(__FILE__), 'config.yml')
+  enable :logging
+  enable :prefixed_redirects
+
+  configure do
+    set :client, MirlynIdApi::SolrClient.new("http://mojito.umdl.umich.edu:8026/solr/biblio")
+  end
+
+  get '/' do
+    json({'valid_keys' => {
+          'id' => 'Mirlyn Record ID',
+          'issn' => "ISSN",
+          'oclc' => 'OCLC Number',
+          'isbn' => "ISBN",
+          'lccn' => "Library of Congress Call Number"
+         }})
+  end
+
+  get '/:key/:val' do
+     resp = settings.client.kv_search(params[:key], params[:val])
+     if resp.empty?
+       status 404
+       json resp.to_h
+     else
+       json resp.to_h
+     end
+  end
+
+  run! if app_file == $0
+end
