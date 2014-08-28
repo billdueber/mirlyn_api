717d73d042a852ed2c1d401c2acd7b65e45073b1
Bill Dueber <bill@dueber.com>
2014-08-27 22:33:54 -0400

    Do redirects on old API methods: full/brief simple

--- config.ru
+++ config.ru
@@ -1,6 +1,10 @@
 require './simple_mirlyn_api'
 
+
 map '/simple/v1' do
   run SimpleMirlynAPI
 end
 
+map '/volumes' do
+  run OldAPIRedirect
+end
--- simple_mirlyn_api.rb
+++ simple_mirlyn_api.rb
@@ -4,6 +4,22 @@ require 'sinatra/jsonp'
 require 'json'
 require 'mirlyn_id_api'
 
+class OldAPIRedirect < Sinatra::Base
+
+  enable :logging
+
+# Redirect old volumes stuff
+#  RewriteRule   ^/api/volumes/(full|brief)/([^/]+)/(.+)[.](json)$ static/api/volumes.php?q=$2:$3&type=$\4&single=1&brevity=$1 [QSA,L]
+
+  get '/:full_brief/:key/*.json' do |brev, key, id|
+    status 303
+    redirect "http://mirlyn.lib.umich.edu/static/api/volumes.php?q=#{key}:#{id}&type=json&single=1&breviey=#{brev}"
+  end
+
+
+end
+
+
 class SimpleMirlynAPI < Sinatra::Base
 
   register Sinatra::ConfigFile
